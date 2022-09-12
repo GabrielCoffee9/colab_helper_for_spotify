2b32acbee04eb23bafe5b99df687fcb2ea9d4921
Gabriel Café <63719840+GabrielCoffee9@users.noreply.github.com>
2022-09-11 22:13:08 -0300

    Saving 3
    Planning home screen

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -20,30 +20,134 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final controller = context.watch<UserController>();
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: (() {}), child: const Icon(Icons.add)),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(label: 'Home', icon: Icon(Icons.home_outlined)),
+          NavigationDestination(
+              label: 'Search', icon: Icon(Icons.search_outlined)),
+          NavigationDestination(
+              label: 'Add', icon: Icon(Icons.add_circle_outline)),
+          NavigationDestination(
+            label: 'Settings',
+            icon: Icon(Icons.settings_outlined),
+          ),
+        ],
+        selectedIndex: 1,
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Welcome ${controller.userProfile.displayName!}'),
-            Center(
-              child: controller.userProfile.images?.first.url == null
-                  ? const CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      child: Icon(
-                        Icons.person_outline_rounded,
-                        size: 36,
-                        color: Colors.black,
-                      ))
-                  : CircleAvatar(
-                      radius: 64,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(32),
-                        child: Image.network(
-                            controller.userProfile.images?.first.url ?? ''),
-                      )),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      controller.userProfile.images?.first.url == null
+                          ? const CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              child: Icon(
+                                Icons.person_outline_rounded,
+                                size: 32,
+                                color: Colors.black,
+                              ))
+                          : CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              radius: 32,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: Image.network(loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                  if (loadingProgress == null) {
+                                    return child;
+                                  }
+                                  return Center(
+                                    child: CircularProgressIndicator(
+                                      value:
+                                          loadingProgress.expectedTotalBytes !=
+                                                  null
+                                              ? loadingProgress
+                                                      .cumulativeBytesLoaded /
+                                                  loadingProgress
+                                                      .expectedTotalBytes!
+                                              : null,
+                                    ),
+                                  );
+                                },
+                                    controller.userProfile.images?.first.url ??
+                                        ''),
+                              )),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(controller.userProfile.displayName!),
+                          Text(
+                            'Spotify ${controller.userProfile.product}',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 52,
+                        width: 54,
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStatePropertyAll<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.notifications_none_outlined,
+                            size: 24,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SizedBox(
+                        height: 52,
+                        width: 54,
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStatePropertyAll<OutlinedBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Icon(
+                            Icons.message_outlined,
+                            size: 24,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide.none),
+                      hintText: 'Search anything you want',
+                      prefixIcon: const Icon(Icons.search_outlined)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
